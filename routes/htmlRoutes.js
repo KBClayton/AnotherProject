af3e7d38c796e86f8371f02a777cfd8fde367d24
jamie reaves <jamiereaves@utexas.edu>
2018-07-21 13:56:20 -0500

    commiting to checkout

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,19 +3,19 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.user.findAll({}).then(function(result) {
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+        //msg: "Welcome!",
+        //examples: result
       });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  // Load jobDetail page and pass in a specific job by id
+  app.get("/jobs/:id", function(req, res) {
+    db.savedJob.findOne({ where: { id: req.params.id } }).then(function(result) {
+      res.render("jobDetails", {
+        //example: result
       });
     });
   });
