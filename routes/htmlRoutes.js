ced12c9950562e49c45273a471581e168af6084e
Sunshine Daydream <alexbruner@live.com>
2018-07-22 02:45:48 -0500

    built create new user post and handlebar page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -21,6 +21,16 @@ module.exports = function(app) {
     });
   });
 
+  // CreateNewUser Page --Alex
+  app.get("/createProfile", function(req, res) {
+    db.user.findAll({}).then(function(result) {
+      res.render("newProfile", {
+        msg: "Welcome!",
+        examples: result
+      });
+    });
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
