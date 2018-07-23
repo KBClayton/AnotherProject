bfe6dda53ce1b8fa9ae8f5c7efdb9a9407c01d81
Sunshine Daydream <alexbruner@live.com>
2018-07-22 19:45:40 -0500

    added login page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,13 +6,13 @@ var path = require("path");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    //db.user.findAll({}).then(function(result) {
+    // db.user.findAll({}).then(function(result) {
       res.render("index", {
         msg: "Welcome!",
         examples: result
       });
     });
-  //});
+//  }); 
 
   // index route loads view.html
   //app.get("/", function(req, res) {
