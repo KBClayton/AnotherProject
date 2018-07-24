27119d3a4e328daef4b51e6839e74b015c81ca49
Alan <andrewsalan88@gmail.com>
2018-07-23 21:25:37 -0500

    made changes to jobDetails.handlebars

--- public/js/jobDetails.js
+++ public/js/jobDetails.js
@@ -1,12 +1,35 @@
 
 
-
-$( document ).ready(function() {
-  console.log( "ready!" );
-
+$(".addCommentBtn").on("click", function(event){
+  console.log($(this).data("id"));
+  var commentID = {
+    savedJobID: $(this).data("id"),
+    comment: $(".commentBox").val()
+  }
   
-
+  console.log(commentID);
+  $.ajax("/api/jobs", {
+    type: "POST",
+    data: commentID
+    }).then(
+    function(){
+      console.log("Created New Job Lead");
+    }
+  );
+  
 });
+
+
+
+
+// $( document ).ready(function() {
+//   console.log( "ready!" );
+
+
+
+// });
+
+
 
 // $.ajax("/api/jobs", {
 //   type: "GET",
