f8c5ab6f155689bfb6ff10ade9e002c28a3735de
Sunshine Daydream <alexbruner@live.com>
2018-07-23 22:18:27 -0500

    changed testroutes api/all/:id to findone

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -22,7 +22,7 @@ module.exports = function(app) {
   });
   // -- Get All Data From A Specific UserID (Join)
   app.get("/api/all/:id?", function(req, res) {
-    db.user.findAll({
+    db.user.findOne({
       where: {
         id: req.params.id
       },
