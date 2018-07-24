4f54f12d56a3f787e6b8de58430239a13f6fd587
KBClayton <clayton.kevinb@gmail.com>
2018-07-24 14:48:35 -0500

    stuff

--- public/js/login.js
+++ public/js/login.js
@@ -10,9 +10,17 @@ $("#signInBtn").on("click", function(){
    $.ajax("/api/login", {
      type: "POST",
      data: LoginAccess
-   }).then(
-    function(){
-      console.log("login");
+   })
+   .then(function(res){
+    console.log("in then client side");
+    console.log(res);
+    if(res.status==200){
+      window.location = res;
+    }else{
+      alert(res);
+    }
+     //location.href = res;
+      //console.log("login");
     }
    )
 })
