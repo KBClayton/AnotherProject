2c7dcecc2b4ee443653e32632c077446a40c08f2
Alan <andrewsalan88@gmail.com>
2018-07-26 22:06:44 -0500

    lots of changes

--- public/js/editJob.js
+++ public/js/editJob.js
@@ -13,4 +13,30 @@ $( document ).ready(function() {
       console.log("Heyo");
     }
   );
+
+  // var contents = $('#chComp')[0].placeholder;
+  // console.log(contents);
+  // console.log(document.getElementById('#chCompany').getAttribute('value'));
+});
+
+
+$("#saveCompChanges").on("click", function(event){
+  var contents = $('#chComp')[0].placeholder;
+  console.log(contents);
+  // console.log($(this).data("id"));
+  // var commentID = {
+  //   savedJobID: $(this).data("id"),
+  //   comment: $(".commentBox").val()
+  // }
+  
+  // console.log(commentID);
+  // $.ajax("/api/jobs", {
+  //   type: "POST",
+  //   data: commentID
+  //   }).then(
+  //   function(){
+  //     console.log("logged job id");
+  //   }
+  // );
+  
 });
